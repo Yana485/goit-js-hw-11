cd63498587c0440d0d9d719246cd04cb7f5cd080
Yana <105270905+Yana485@users.noreply.github.com>
2026-06-17 14:26:44 +0100

    trying to get a response from this

--- src/main.js
+++ src/main.js
@@ -14,12 +14,12 @@ searchButton.addEventListener("click", function (event) {
         container.innerHTML = '';
         getImagesByQuery(searchField.value)
           // .then(value => { console.log(value) })
-          .then(response => {
-            console.log(`response.length = ${response.length}`);
+          .then(value => {
+            console.log(`response.length = ${value.length}`);
             if (response.length > 0) {
-                console.log(`I'm going to call createGallery now: ${response}`);
+                console.log(`I'm going to call createGallery now: ${value}`);
                 // return response.data.hits;
-                container.insertAdjacentHTML("beforeend",createGallery(response));
+                return container.insertAdjacentHTML("beforeend",createGallery(value));
             } else {
                 throw new Error();
             }
